fd2914c4507304b374c6cb1f1769e20b8f113a32
King Elisha <ek.chibueze@gmail.com>
2020-03-23 23:51:49 +0100

    responsive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 const menu = document.getElementById("menu");
+const menuContent = document.getElementById("menu-content");
+const nav = document.getElementById("nav");
 
 menu.addEventListener("click", e => {
   menu.classList.toggle("open");
+  menuContent.classList.toggle("open");
+  setTimeout(() => {
+    nav.classList.toggle("open");
+  }, 300);
 });
